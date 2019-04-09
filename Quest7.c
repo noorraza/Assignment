234a93ca546dcc71562d01d20be6065fd7e04e41
NOOR RAZA <noor.raza1998@gmail.com>
2019-04-10 01:54:01 +0530

    Update Quest7.c
    made an structure for process ID and took IO_term as input output interaction

--- Quest7.c
+++ Quest7.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
-#include<unistd.h>
+/*Researchers designed one system that classified interactive and noninteractive processes
+automatically by looking at the amount of terminal I/O. If a process did not input or
+output to the terminal in a 1-second interval, the process was classified as noninteractive
+and was moved to a lower-priority queue. In response to this policy, one programmer
+modified his programs to write an arbitrary character to the terminal at regular intervals
+of less than 1 second. The system gave his programs a high priority, even though the
+terminal output was completely meaningless.*/
 
-int main()
+
+
+#include<unistd.h>
+#include<stdio.h>
+
+struct Process
 {
-	
-}
+	int process_id;
+	int IO_term;
+};
